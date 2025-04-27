a4736cd5a62cb6f526ede307539e9dba83d287a2
c0da3n <sky@codawn.dev>
2025-04-27 11:15:03 -0400

    Feat: Create Splash Screen & Font Setting

---
+++ HT/Font/CustomFont.swift
@@ -0,0 +1,22 @@
+//
+//  CustomFont.swift
+//  HT
+//
+//  Created by Jungi Min on 4/27/25.
+//
+
+import SwiftUI
+
+extension Font {
+    // Semibold
+    static let PretendardSemiBold20: Font = .custom("Pretendard-Semibold", size: 20)
+    static let PretendardSemiBold16: Font = .custom("Pretendard-Semibold", size: 16)
+    
+    // Regular
+    static let PretendardRegular14: Font = .custom("Pretendard-Regular", size: 14)
+    
+    // Medium
+    static let PretendardMedium14: Font = .custom("Pretendard-Medium", size: 14)
+
+}
+
--- HT/HTApp.swift
+++ HT/HTApp.swift
@@ -12,6 +12,7 @@ struct HTApp: App {
     var body: some Scene {
         WindowGroup {
             ContentView()
+                .preferredColorScheme(.light)
         }
     }
 }
